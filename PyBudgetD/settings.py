97a84f53e2fd505b45f31751a98d8dbb90eebacb
tami haimov <tamihaimov@gmail.com>
2018-07-24 21:54:21 +0300

    static

--- PyBudgetD/settings.py
+++ PyBudgetD/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    # I have the static folder inside my app and not inside the project
+    os.path.join(BASE_DIR, 'budgetApp/static'),
+)
